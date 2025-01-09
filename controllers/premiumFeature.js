fd01c7c223cd41ebb5bd4405acd4b4553baa44b7
Subhrajitsarkar <subhrajitsarkar28@gmail.com>
2025-01-09 13:01:33 +0530

    12. Final Leaderboard Optimisation

--- controllers/premiumFeature.js
+++ controllers/premiumFeature.js
@@ -5,24 +5,23 @@ let sequelize = require('../utils/database')
 const getUserLeaderBoard = async (req, res) => {
     try {
         let leaderboardofusers = await User.findAll({
-            attributes: ['id', 'name', [sequelize.fn('sum', sequelize.col('expenses.price')), 'total_cost']],
+            attributes: [
+                'id',
+                'name',
+                [sequelize.fn('COALESCE', sequelize.fn('sum', sequelize.col('expenses.price')), 0), 'total_cost']],
             include: [
                 {
                     model: Expense,
                     attributes: []
                 }
             ],
-            group: ['user.id'], // Ensure grouping matches the user alias
-            order: [['total_cost', 'DESC']] //(or) order: [[sequelize.literal('total_cost'), 'DESC']], // Sort by aggregated column
+            group: ['user.id'], // Group by user ID
+            order: [[sequelize.literal('total_cost'), 'DESC']] // Order by total cost
         })
-        // Check if leaderboard has any data
-        if (leaderboardofusers.length === 0) {
-            return res.status(200).json({ message: "No data found", leaderboard: [] });
-        }
         res.status(200).json(leaderboardofusers);
 
     } catch (err) {
-        console.log(err);
+        console.error("Error fetching leaderboard:", err.message);
         res.status(500).json(err)
     }
 }
